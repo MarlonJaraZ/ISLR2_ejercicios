a219caa591a7faec0181a470a33f4153ef2c19c5
Marlon Jaramillo <marlonja@hotmail.com>
2021-11-30 10:29:33 -0500

    Inicio

---
+++ Scripts/cap_2.R
@@ -0,0 +1,20 @@
+
+# Programa-------------------------------------------------------------------------
+# <<ejercicios ISLR2>>
+
+
+# Descripcion: 
+# Ejercicios libro ISLR2
+# Autor: 
+# Marlon Jaramillo Zapata <mjaramilloz@superservicios.gov.co>
+# Version: 1.0
+# [30.11.2021]
+
+
+# librerias ---------------------------------------------------------------
+
+require(ISLR2)
+#install.packages("ISLR2")
+
+library(ISLR2)
+
